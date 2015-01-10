311064fd0759a862fd08abec0a3b13258cde7b14
alexbid <bidault@hotmail.fr>
2015-01-10 23:25:24 +0100

    first alex

--- hellodjango/views.py
+++ hellodjango/views.py
@@ -7,9 +7,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print r.text
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    #r = requests.get('http://httpbin.org/status/418')
+    r = "Salut Alex"
+    #print r.text
+    #return HttpResponse('<pre>' + r.text + '</pre>')
+    print r
+    return HttpResponse('<pre>' + r + '</pre>')
 
 
 def db(request):
