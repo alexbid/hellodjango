56b9e36e521858c5e6af18e9b347042e0b8010be
U-CONIX\Alexandre.BIDAULT <Alexandre.BIDAULT@R2M-WK-DIRG-001.conix.corp>
2015-01-14 14:05:07 +0100

    first try printing spot from DB

--- hellodjango/views.py
+++ hellodjango/views.py
@@ -1,14 +1,37 @@
 import requests
+import sqlite3
 
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # from .models import Greeting
+import os
+output = os.path.dirname(__file__)
+if not output: portfolioDB  = 'portfolio.db'
+else:
+        portfolioDB = output + '/portfolio.db'
+        output += "/"
+
+# request data here
+def getLastClose():
+	BBG = '^FCHI'
+	flag = 'close'
+	conn = sqlite3.connect(portfolioDB)
+	c = conn.cursor()
+	c.execute('SELECT spot FROM (SELECT  MAX(date), spot FROM spots WHERE BBG = ? and flag = ?)', (BBG, flag))
+
+	data = c.fetchone()[0]
+	if data == 0:
+		conn.close()
+		return 'check your data, no spot available'
+	else:
+        	conn.close()
+		return data
 
 # Create your views here.
 def index(request):
     #r = requests.get('http://httpbin.org/status/418')
-    r = "Salut Alex"
+    r = "Salut Alex" + getLastClose()
     #print r.text
     #return HttpResponse('<pre>' + r.text + '</pre>')
     print r
@@ -23,3 +46,5 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, 'db.html', {'greetings': greetings})
+
+
