fd0acf68268e75ee98d1c3f0fe97e71bf63dc8b9
alexbid <bidault@hotmail.fr>
2015-03-14 22:37:38 +0100

    from my snow leopard

--- hellodjango/views.py
+++ hellodjango/views.py
@@ -48,7 +48,7 @@ def index(request):
 	from worker import conn
 
 	q = Queue(connection=conn)
-	And enqueue the function call:
+	#And enqueue the function call:
 	from utils import count_words_at_url
 
 	result = q.enqueue(count_words_at_url, 'http://heroku.com')
