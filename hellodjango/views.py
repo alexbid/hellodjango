b1d71780f156a0317d5ea3f820f4f888a36bedff
alexbid <bidault@hotmail.fr>
2015-03-14 22:45:04 +0100

    from my snow leopard

--- hellodjango/views.py
+++ hellodjango/views.py
@@ -51,7 +51,6 @@ def index(request):
 	#And enqueue the function call:
 	from utils import count_words_at_url
 	result = q.enqueue(count_words_at_url, 'http://heroku.com')
-	
 	r = str(getLastClose())
     #print r
     return HttpResponse('<pre>' + r + result + '</pre>')
