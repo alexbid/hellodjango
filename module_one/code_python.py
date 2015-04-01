f2e079527b3f13482df413ee7d90b153f0535ba9
alexbid <bidault@hotmail.fr>
2015-04-01 23:23:07 +0200

    from my SnowLeopard

--- module_one/code_python.py
+++ module_one/code_python.py
@@ -243,7 +243,10 @@ class Portfolio:
 		#print endDate, type(endDate)
 		if sqlConn.bSqlite3: c.execute('SELECT date, trans, BBG, qty, price, broker FROM trades WHERE (date BETWEEN ? AND ?)',(stDate, endDate))
 #		if sqlConn.bPostgre: c.execute("""SELECT date, trans, BBG, qty, price, broker FROM trades WHERE (date BETWEEN %(date)s AND %(date)s)""", [datetime.date(stDate), datetime.date(endDate)])
-		if sqlConn.bPostgre: c.execute("SELECT date, trans, BBG, qty, price, broker FROM trades WHERE (BBG = '^FCHI');")
+		
+		
+		pypy = '^FCHI'
+		if sqlConn.bPostgre: c.execute("SELECT date, trans, BBG, qty, price, broker FROM trades WHERE (BBG = %s);", pypy)
 
 		#if sqlConn.bPostgre: c.execute("""SELECT date, trans, BBG, qty, price, broker FROM trades WHERE (date BETWEEN %s AND %s);""", {'date': stDate, 'date': endDate})
 		#if sqlConn.bPostgre: c.execute('SELECT date, trans, BBG, qty, price, broker FROM trades WHERE (date BETWEEN %(date)s AND %(date)s)', (stDate, endDate))
