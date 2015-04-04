16338937842f4fc7b39ad71682eec301cc82eee5
alexbid <bidault@hotmail.fr>
2015-04-05 00:32:44 +0200

    from my snowleopard with my new Python

--- hellodjango/views.py
+++ hellodjango/views.py
@@ -47,7 +47,7 @@ def getLastClose():
 		#except: return "failed to connect to DB....."
 		c = conn.cursor()
 		#c.execute('SELECT spot FROM (SELECT  MAX(date), spot FROM spots WHERE BBG = %s and flag = %s) AS FOO', (BBG, flag))
-		c.execute("SELECT spot FROM spots WHERE date=(SELECT MAX(date) FROM spots WHERE BBG = %s AND flag = %s) AND BBG = $s AND flag = %s", (BBG, flag, BBG, flag))
+		c.execute("(SELECT spot FROM spots WHERE (date=(SELECT MAX(date) FROM spots WHERE BBG = %s AND flag = %s) AND BBG = $s AND flag = %s));", (BBG, flag, BBG, flag))
 		
 
 		data = c.fetchone()[0]
