23c9a161e6b396902fc2c7b4265866e630f02857
alexbid <bidault@hotmail.fr>
2015-04-05 00:39:37 +0200

    from my snowleopard with my new Python

--- toto.py
+++ toto.py
@@ -6,7 +6,7 @@ from module_one.code_python import vTradingDates
 from module_one.code_python import doRequestData
 
 dt = datetime.date(2014, 12, 30)
-end = datetime.date(2015, 01, 05)
+end = datetime.date(2015, 12, 31)
 
 from timeit import Timer
 #t = Timer(lambda: vTradingDates(dt, end, 'FR'))
