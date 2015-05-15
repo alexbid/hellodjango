af5efaaa90b25c88307dafd19245f1a03b0b4cd7
alexbid <bidault@hotmail.fr>
2015-05-16 00:51:36 +0200

    from my snowleopard with my new Python

--- hellodjango/urls.py
+++ hellodjango/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from hellodjango import views
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,7 +11,9 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index, name='index'),
-	url(r'^static/(.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT})
+	#url(r'^static/(.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT})
 	#url(r'^$', 'hellodjango.myapp.views.home', name='home'),
 	
 )
+
+urlpatterns += staticfiles_urlpatterns()
