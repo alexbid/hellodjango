ed5b8d8b45a329c0eaa0e00c66eefc78b05aca55
alexbid <bidault@hotmail.fr>
2015-05-28 21:49:31 +0200

    from my snowleopard with my new Python

--- install_machine.sh
+++ install_machine.sh
@@ -1,4 +1,5 @@
 mkdir /home/ubuntu/hellodjango
+mkdir /home/ubuntu/downloads
 cd hellodjango
 sudo apt-get install git
 git init
@@ -8,7 +9,7 @@ apt-get install libcurl4-gnutls-dev libexpat1-dev gettext \ libz-dev libssl-dev
 #Install Python
 sudo apt-get install build-essential
 sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
-cd ~/Downloads/
+cd /home/ubuntu/downloads
 wget https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
 tar -xvf Python-2.7.10.tgz
 cd Python-2.7.10
