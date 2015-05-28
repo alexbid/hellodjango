4e8b2fc305de0fa0abf1c0fa8db03116df5ae85b
alexbid <bidault@hotmail.fr>
2015-05-28 21:46:04 +0200

    from my snowleopard with my new Python

--- install_machine.sh
+++ install_machine.sh
@@ -4,6 +4,18 @@ sudo apt-get install git
 git init
 git pull https://github.com/alexbid/hellodjango.git
 apt-get install libcurl4-gnutls-dev libexpat1-dev gettext \ libz-dev libssl-dev
-sudo apt-get install python 2.7.10
+
+#Install Python
+sudo apt-get install build-essential
+sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+cd ~/Downloads/
+wget https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
+tar -xvf Python-2.7.10.tgz
+cd Python-2.7.10
+./configure
+make
+sudo make install
+
+#sudo apt-get install python 2.7.10
 sudo pip install -r requirements.txt
 
