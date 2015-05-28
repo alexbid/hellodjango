121bd5814e9d658d5e9364fc28d022a660cac1ce
alexbid <bidault@hotmail.fr>
2015-05-28 21:35:56 +0200

    from my snowleopard with my new Python

--- install_machine.sh
+++ install_machine.sh
@@ -1,7 +1,9 @@
-
+mkdir /home/ubuntu/hellodjango
+cd hellodjango
+sudo apt-get install git
+git init
+git pull https://github.com/alexbid/hellodjango.git
 apt-get install libcurl4-gnutls-dev libexpat1-dev gettext \ libz-dev libssl-dev
 sudo apt-get install python 2.7.10
-sudo apt-get install git
-
 sudo pip install -r requirements.txt
 
