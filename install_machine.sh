26d3b2a9fe97fb5f3689ca54cfecb75205e215fd
alexbid <bidault@hotmail.fr>
2015-05-28 21:26:03 +0200

    from my snowleopard with my new Python

---
+++ install_machine.sh
@@ -0,0 +1,7 @@
+
+apt-get install libcurl4-gnutls-dev libexpat1-dev gettext \ libz-dev libssl-dev
+sudo apt-get install python 2.7.10
+sudo apt-get install git
+
+sudo pip install -r requirements.txt
+
