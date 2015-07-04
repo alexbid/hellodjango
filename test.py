29fc3cfe958edd6df4183a9664df3bae8eca224a
Alexandre Bidault <bidault@hotmail.fr>
2015-07-04 21:14:41 +0200

    from BIBID

--- test.py
+++ test.py
@@ -6,7 +6,9 @@ from pandas import *
 from pandas.io.json import json_normalize
 import pandas as pd
 from datetime import timedelta
-
+#from datetime import gmtime
+from datetime import tzinfo
+import pytz 
 
 df = pd.read_json("http://www.bloomberg.com/markets/chart/data/1D/CAC:IND")
 shift = 6
@@ -21,13 +23,16 @@ shift = 6
 
 
 #print df['exch_open_time'][0]+ timedelta(hours=shift), df['exch_close_time'][0]+ timedelta(hours=shift)
-print df['exch_open_time'][0]+ timedelta(hours=shift), df['exch_close_time'][0]+ timedelta(hours=shift)
+print df['exch_open_time'][0]+ timedelta(hours=shift), df['exch_close_time'][0] + timedelta(hours=shift)
 
 #print df['data_values']
+toot = pytz.timezone('Europe/Paris')
 
 for points in df['data_values']:
     #tdate = datetime.fromtimestamp(points[0]/1000) + timedelta(hours=shift-2)
-    tdate = datetime.fromtimestamp(points[0]/1000) - datetime.utcfromtimestamp(points[0]/1000)
-    print tdate, float(points[1])
-    print tdate, float(points[1])
+    #tdate = datetime.fromtimestamp(points[0]/1000) - datetime.utcfromtimestamp(points[0]/1000)
+	tdate = datetime.utcfromtimestamp(points[0]/1000)
+	print tdate, float(points[1]), datetime.fromtimestamp(points[0]/1000, tz=toot )
+	#print tdate, float(points[1])
 
+#print datetime.gmtime(), datetime.localtime()
