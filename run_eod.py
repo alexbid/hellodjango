192b88ed194528db4d9d22843c4c046e2d1d4388
alexbid <bidault@hotmail.fr>
2015-07-05 19:44:26 +0200

    from my snowleopard with my new Python

--- run_eod.py
+++ run_eod.py
@@ -10,41 +10,59 @@ import numpy as np
 
 shift = 6
 
-df = pd.read_json("http://www.bloomberg.com/markets/chart/data/1D/CAC:IND")['data_values']
-df = np.array(df)
-
 sqlConn = sqlConnector()
 c = sqlConn.conn.cursor()
-dateArray = []
-spotArray = []
 
-for points in df:
-#    tdate = datetime.fromtimestamp(points[0]/1000) + timedelta(hours=shift)
-    tdate = datetime.utcfromtimestamp(points[0]/1000) + timedelta(hours=shift)
-    dateArray.append(tdate)
-    spotArray.append(float(points[1]))
+def bloombergScrap(mnemo, ric):
+
+	df = pd.read_json("http://www.bloomberg.com/markets/chart/data/1D/" + mnemo)['data_values']
+	df = np.array(df)
+
+	dateArray = []
+	spotArray = []
+
+	for points in df:
+	#    tdate = datetime.fromtimestamp(points[0]/1000) + timedelta(hours=shift)
+		tdate = datetime.utcfromtimestamp(points[0]/1000) + timedelta(hours=shift)
+		dateArray.append(tdate)
+		spotArray.append(float(points[1]))
    
-s = pd.DataFrame(spotArray, index = dateArray, columns=['Last'])
-s.index.name = 'Date'
-s['bbg'] = '^FCHI'
+	s = pd.DataFrame(spotArray, index = dateArray, columns=['Last'])
+	s.index.name = 'Date'
+	s['bbg'] = ric
 
-minDate = s.index.min()
-maxDate = s.index.max()
+	minDate = s.index.min()
+	maxDate = s.index.max()
 
-db = pd.read_sql("""SELECT "Date", "Last","bbg" FROM intraday WHERE ("Date" BETWEEN %s AND %s) ORDER BY "Date" ASC""", sqlConn.conn, index_col="Date", parse_dates=True, params=(minDate.strftime("%Y-%m-%d %H:%M:%S"), maxDate.strftime("%Y-%m-%d %H:%M:%S")))
+	db = pd.read_sql("""SELECT "Date", "Last","bbg" FROM intraday WHERE ("bbg"=%s AND ("Date" BETWEEN %s AND %s)) ORDER BY "Date" ASC""", sqlConn.conn, index_col="Date", parse_dates=True, params=(ric, minDate.strftime("%Y-%m-%d %H:%M:%S"), maxDate.strftime("%Y-%m-%d %H:%M:%S")))
 
-toto = np.array(pd.to_datetime(db.index))
-tutu = np.array(pd.to_datetime(s.index))
+	toto = np.array(pd.to_datetime(db.index))
+	tutu = np.array(pd.to_datetime(s.index))
+	missingDates = np.setdiff1d(tutu, toto)
 
-missingDates = np.setdiff1d(tutu, toto)
+	print 'missingDates for ' + mnemo + ': ', missingDates
+	toDB = pd.DataFrame(s, index=missingDates)# , how='outer') #, lsuffix='_left', rsuffix='_right')
+	toDB.index.name = 'Date'
 
-print 'missingDates: ', missingDates
-toDB = pd.DataFrame(s, index=missingDates)# , how='outer') #, lsuffix='_left', rsuffix='_right')
-toDB.index.name = 'Date'
+	engine = create_engine('postgres://awsuser:Newyork2012@awsdbinstance.c9ydrnvcm8aj.us-west-2.rds.amazonaws.com:5432/marketdb')
+	try:
+		toDB.to_sql('intraday', engine, if_exists='append') 
+	except psycopg2.IntegrityError:
+		print "quote already in DB Funds"
+	print mnemo + '....done'
+	
+mnemoList = pd.read_sql("""SELECT DISTINCT "mnemo", "BBG" FROM batch_run WHERE "mnemo" IS NOT NULL ORDER BY "mnemo" ASC""", sqlConn.conn)	
+#print mnemoList
+for index, row in mnemoList.iterrows():
+	bloombergScrap(row['mnemo'], row['BBG'])
 
-engine = create_engine('postgres://awsuser:Newyork2012@awsdbinstance.c9ydrnvcm8aj.us-west-2.rds.amazonaws.com:5432/marketdb')
-try:
-    toDB.to_sql('intraday', engine, if_exists='append') 
-except psycopg2.IntegrityError:
-	print "quote already in DB Funds"
-print '....done'
+
+#for imnemo in mnemoList['mnemo']:
+	#bloombergScrap(imnemo)
+
+
+
+
+
+
+
