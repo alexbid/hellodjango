561122952cac7a5f22b080420e936a6d260ef296
alexbid <bidault@hotmail.fr>
2016-06-23 22:37:18 +0200

    from my SnowLeopard

--- hellodjango/apache/wsgi.py
+++ hellodjango/apache/wsgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 
 import os, sys
 sys.path.append('/home/pi/hellodjango')
+sys.path.append('/home/pi/')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellodjango.settings")
 
 import django.core.handlers.wsgi
