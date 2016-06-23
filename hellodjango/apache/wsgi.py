5ebdeeb62624089b78ec538f75d99ecddd704cfd
alexbid <bidault@hotmail.fr>
2016-06-23 22:21:52 +0200

    from my SnowLeopard

--- hellodjango/apache/wsgi.py
+++ hellodjango/apache/wsgi.py
@@ -7,8 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+sys.path.append('/home/pi/hellodjango')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellodjango.settings")
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
+
+#from django.core.wsgi import get_wsgi_application
+#application = get_wsgi_application()
