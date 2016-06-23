201597d2d091f980cec1421240465086b3a9b657
alexbid <bidault@hotmail.fr>
2016-06-24 00:04:32 +0200

    from my SnowLeopard

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -26,12 +26,9 @@ STATICFILES_DIRS = (
 SECRET_KEY = 'pres36m7pn(#gc_ho^jz*1ilephs#9gz4fp#2)^a5xcp7*=)lx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = False
 TEMPLATE_DEBUG = True
-
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -62,14 +59,6 @@ WSGI_APPLICATION = 'hellodjango.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-#DATABASES['default'] =  dj_database_url.config()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
--- settings.py
+++ settings.py
@@ -2,23 +2,8 @@
 import dj_database_url
 #import os
 DATABASES['default'] =  dj_database_url.config()
-#DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
-#print os.environ.get('LOCAL_DEV', False), "Alex!!!!"
-#if bool(os.environ.get('LOCAL_DEV', False)):
-#if not os.environ.has_key('DATABASE_URL'):
-#    os.environ['DATABASE_URL'] = 'postgres://localhost'
-#DATABASES = {'default': dj_database_url.config(default=os.environ['DATABASE_URL'])}
 
 # Override DATABASES['default'] with your local database configuration
-
-#try:
-#  database_url = os.environ["DATABASE_URL"]
-#except KeyError:
-#  database_url = "file:///{}".format(os.path.join(BASE_DIR, 'db.sqlite3'))
-#
-#DATABASES = { 'default': dj_database_url.config() }
-#
-#DATABASES = {'default': dj_database_url.config(default='postgres://wcmikblybrgqbz:ZycOXg48gWJlRGR3MVFA9qGxvB@ec2-23-23-210-37.compute-1.amazonaws.com:5432/d3ibjjmjb9fqrm')}
 
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
