74c1d221ec1ce3cd3ce15f6dcaa9767e75bd6b8d
alexbid <bidault@hotmail.fr>
2016-10-07 20:58:15 +0200

    from my SnowLeopard

--- batchs/run_nav.py
+++ batchs/run_nav.py
@@ -19,7 +19,8 @@ from lxml import etree
 x = universe.Universe()
 univers = x.load_fund_nav()
 session = requests.Session()
-headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit 537.36 (KHTML, like Gecko) Chrome", "Accept":"text/html,application/xhtml+xml,application/xml; q=0.9,image/webp,*/*;q=0.8"}
+
+headers = {"User-Agent":"Mozilla/ 5.0 (Macintosh; Intel Mac OS X 10_9_5)   AppleWebKit 537.36 (KHTML, like Gecko) Chrome", "Accept":"text/html, application/xhtml+xml, application/xml; q=0.9, image/webp,*/*;q=0.8"}
 
 for idx in range(len(univers)):
 	wkn = str(univers['wkn'].ix[idx])
