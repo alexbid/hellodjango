4a5103f7cd0cc2b00767387749383644ccd72e58
alexbid <bidault@hotmail.fr>
2016-11-22 22:36:09 +0100

    from my SnowLeopard

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from django.conf import settings
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 
