1150ab9b3bda5ba8cde156a2a306ea36e4150906
alexbid <bidault@hotmail.fr>
2016-11-22 22:59:07 +0100

    from my SnowLeopard

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -16,7 +16,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 STATICFILES_DIRS = (
     os.path.join(SITE_ROOT, 'static/'),
